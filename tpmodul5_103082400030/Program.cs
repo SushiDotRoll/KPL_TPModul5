d133851f16f7cfb25be4183b5a260a71d7d6360d
SushiDotRoll <irsyad.spramudito@gmail.com>
2026-03-29 21:13:16 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_103082400030/Program.cs
+++ tpmodul5_103082400030/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+
+        halo.SapaUser("Irsyad");
+    }
+}
